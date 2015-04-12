cfbea53caa340e7d7bd4cf20a7b3470b4407ddf1
Bjoern Klatt <bk@puerapuliae.net>
2015-04-12 15:47:25 +0200

    provided jessie configs

---
+++ late_command.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# passwordless sudo
+echo "%sudo   ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# public ssh key for vagrant user
+mkdir /home/vagrant/.ssh
+wget -O /home/vagrant/.ssh/authorized_keys "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
+chmod 755 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# speed up ssh
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# display login promt after boot
+sed "s/quiet splash//" /etc/default/grub > /tmp/grub
+sed "s/GRUB_TIMEOUT=[0-9]/GRUB_TIMEOUT=0/" /tmp/grub > /etc/default/grub
+update-grub
+
+# clean up
+apt-get clean
+rm -rf /usr/share/man/??
+rm -rf /usr/share/man/??_*
+
+# Zero free space to aid VM compression
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
